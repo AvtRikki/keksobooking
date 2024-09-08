d7ff68da7424d5ff2422c2b263d927f156c291e3
bogza.anton <bogza.anton@gmail.com>
2024-09-08 11:46:05 -0700

    Updated form manager to support check in and out time and updating placeholder based on selected housing options

--- js/managers/form-manager.js
+++ js/managers/form-manager.js
@@ -2,6 +2,10 @@ import {FormValidator} from '../validation/form-validator.js';
 
 export class FormManager {
   #SUBMIT_SUFFIX = '__submit';
+  #TIME_LINE_SUFFIX = '__element--time'
+
+  #TIME_IN_SELECTOR = '#timein'
+  #TIME_OUT_SELECTOR = '#timeout'
 
   #UPLOADING_MESSAGE = 'Загрузка...';
 
@@ -9,13 +13,21 @@ export class FormManager {
     this.submitButton = document.querySelector(`.${formName}${this.#SUBMIT_SUFFIX}`);
     this.submitButtonDefaultName = this.submitButton.textContent;
     this.uploadForm = document.querySelector(`.${formName}`);
-
+    this.timeInSelect = this.uploadForm.querySelector(this.#TIME_IN_SELECTOR);
+    this.timeOutSelect = this.uploadForm.querySelector(this.#TIME_OUT_SELECTOR);
+    this.timeLineGroup = this.uploadForm.querySelector(`.${formName}${this.#TIME_LINE_SUFFIX}`);
     this.validator = new FormValidator(this.uploadForm);
 
     this.onFormSubmit = async (evt) => {
       evt.preventDefault();
       await this.#trySubmitFormIfValid(evt.target);
     };
+
+    this.timeLineGroup.addEventListener('change', (evt) => {
+      if (evt.target.value) {
+        this.timeOutSelect.value = this.timeInSelect.value = evt.target.value;
+      }
+    });
   }
 
   async #trySubmitFormIfValid(target) {
